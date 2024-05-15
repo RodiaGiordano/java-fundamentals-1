4859095792c1245a68b902e9f19e76bb2ba59b4d
RodiaGiordano <rodiagiordano93@gmail.com>
2024-05-15 15:10:51 +0200

    Fizz Buzz

---
+++ src/snack/three/FizzBuzz.java
@@ -0,0 +1,26 @@
+package snack.three;
+
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] guestList = { "Jennifer Lopez", "Zendaya", "Chris Hemsworth", "Bad Bunny", "Cara Delevingne", "Kendall Jenner", "Uma Thurman", "Damiano David", "Luca Guadagnino"};
+        System.out.println("nominativo?");
+        String name = scanner.nextLine();
+
+        String response = name + " Non è nella lista";
+
+        for(int i = 0; i < guestList.length; i++){
+            if(name.equals(guestList[i])) {
+                response = name + " E' nella lista";
+                break;
+            }
+        }
+
+        System.out.println(response);
+
+    }
+}
