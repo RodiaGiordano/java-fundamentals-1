bff2ee73faeeef6a62daee680a6b02b5fc15545c
RodiaGiordano <rodiagiordano93@gmail.com>
2024-05-15 16:11:42 +0200

    first refactoring snack two

--- src/snack/two/FavoriteFoods.java
+++ src/snack/two/FavoriteFoods.java
@@ -1,5 +1,7 @@
 package snack.two;
 
+import java.util.Scanner;
+
 public class FavoriteFoods {
     public static void main(String[] args){
         String[] foods = {"pasta", "pizza","verdura", "carne", "pesce", "frutta", "cinese"};
@@ -8,8 +10,31 @@ public class FavoriteFoods {
         System.out.println("Lunghezza: " + arrayLength);
         System.out.println("Preferito: " + foods[0]);
         System.out.println("end: " +  foods[foods.length -1]);
-        System.out.println("mezzo: " + foods[foods.length / 2 -1]);
-    // arrotonda per difetto
+        String half;
+
+        if(foods.length % 2 == 0){
+
+            String smallerHalf = foods[foods.length / 2 -1];
+            String olderHalf = foods[foods.length / 2];
+            boolean choice = false;
+            do{
+            System.out.println("scrivi quale preferisci tra: " + smallerHalf + " e " + olderHalf);
+            Scanner scanner = new Scanner(System.in);
+            half = scanner.nextLine();
+
+            if(half.equals(smallerHalf) || half.equals(olderHalf)){
+                choice = true;
+            }else{
+                System.out.println("Scelta non corretta");
+            }
+            }while(!choice);
+
+        }else{
+            half = foods[foods.length / 2];
+        }
+
+        System.out.println("mezzo: " + half);
+
     }
 }
 
